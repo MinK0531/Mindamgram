28ca820845994dd6eaf76a383ac695e7b04919b4
min <rlaalsrud20807@naver.com>
2025-12-04 15:57:08 +0900

    [김민경] -helloWorld

---
+++ src/main/java/com/mink/mindamgram/MindamgramApplication.java
@@ -0,0 +1,13 @@
+package com.mink.mindamgram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindamgramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MindamgramApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mink/mindamgram/MindamgramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mink.mindamgram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindamgramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
