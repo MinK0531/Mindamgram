41dacbacc6fb08a0fb69e5f49d2124eb453a7dc5
min <rlaalsrud20807@naver.com>
2025-12-04 18:47:54 +0900

    [김민경] -로그인 디자인 중

---
+++ src/main/java/com/mink/mindamgram/user/UserController.java
@@ -0,0 +1,15 @@
+package com.mink.mindamgram.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/user")
+@Controller
+public class UserController {
+
+    @GetMapping("/signup")
+    public String signup(){
+        return "signup";
+    }
+}
